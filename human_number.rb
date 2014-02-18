d7d6604876fae0966b759e14a34bc5b60e8ead22
mikhailov <mikhailov.anatoly@gmail.com>
2014-02-18 09:33:34 +0000

    bugfix

--- human_number.rb
+++ human_number.rb
@@ -14,23 +14,15 @@
 # 1000000 -> "1,000,000"
 # 35235235 -> "35,235,235"
 
-
-n1 = 1
-n2 = 1000
-n3 = 100000
-n4 = 10000000
-n5 = 1000000000
-n6 = 1234567
-
-n = n5
+n = 35235235
 n = n.to_s.reverse
 
 str = ""
 
 
-(1...n.length).each do |i|
-  str += n[i].to_s
-  if i % 3 != 0 || i == n.length - 1
+(1..n.length).each do |i|
+  str += n[i-1].to_s
+  if i % 3 != 0 || i == n.length
     next
   else
     str += ','
